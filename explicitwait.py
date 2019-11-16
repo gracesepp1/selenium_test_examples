4bf9f5d2cb6da51a289995e9802637dd95c746c5
Grace <gracesepp1@gmail.com>
2019-11-16 16:16:05 +0300

    Automated test tutorial 16th

--- explicitwait.py
+++ explicitwait.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome(executable_path=r"C:\Users\user\PycharmProjects\Test\drivers\chromedriver.exe")
 driver.implicitly_wait(5)
@@ -15,9 +18,19 @@ time.sleep(2)
 driver.find_element(By.ID,"flight-destination-hp-flight").send_keys("NYC")
 
 driver.find_element(By.ID,"flight-departing-hp-flight").clear()
-driver.find_element(By.ID,"flight-departing-hp-flight").send_keys("12/10/2018")
+driver.find_element(By.ID,"flight-departing-hp-flight").send_keys("12/12/2019")
 
 driver.find_element(By.ID,"flight-returning-hp-flight").clear()
-driver.find_element(By.ID,"flight-returning-hp-flight").send_keys("15/10/2018")
+driver.find_element(By.ID,"flight-returning-hp-flight").send_keys("15/12/2019")
 
 driver.find_element(By.XPATH,"//*[@id='gcw-flights-form-hp-flight']/div[7]/label/button").click()
+
+#explicit wait
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='stopFilter_stops-0']")))
+
+element.click()
+
+time.sleep(3)
+
+#driver.quit()
