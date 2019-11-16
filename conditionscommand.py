b66f9443fda3723e130e477df57fb1c0825470e8
Grace <gracesepp1@gmail.com>
2019-11-16 12:56:03 +0300

    Added conditions

--- conditionscommand.py
+++ conditionscommand.py
@@ -1,27 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
 
 driver = webdriver.Chrome(executable_path=r"C:\Users\user\PycharmProjects\Test\drivers\chromedriver.exe")
 
 driver.get("http://newtours.demoaut.com/")
 
-print(driver.title)
+user_ele=driver.find_element_by_name('userName')
 
-driver.get("http://pavantestingtools.blogspot.in/")
+print(user_ele.is_displayed())
+print(user_ele.is_enabled())
 
-time.sleep(5)
-
-print(driver.title)
-
-driver.back()
-
-time.sleep(5)
-
-print(driver.title)
-
-driver.forward()
-
-time.sleep(5)
-
-print(driver.title)
